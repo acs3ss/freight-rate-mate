ad0349b2a8e58f367e9d2ab6b8dda166034b848f
Rashid Lasker <rashidlasker@gmail.com>
2018-02-17 10:55:51 -0500

    Realtime routing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*************************************** MAPBOX INITIALIZATION ***************************************/
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFzaGlkbGFza2VyIiwiYSI6ImNqOXh1b2xodjgwdmQycXBhNmpxN21na2cifQ.b7-TzrKTZ3Y_epVuBVynxA';
 
 var map = new mapboxgl.Map({
@@ -8,6 +9,7 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function() {
+    /*************************************** HEATMAP LAYER ***************************************/
     map.addSource('riskdata', {
         "type": "geojson",
         "data": "./data/riskdata.geojson"
@@ -69,20 +71,93 @@ map.on('load', function() {
         }
     }, 'waterway-label');
 
-
-    map.addSource('routes', {
-        'type': 'geojson',
-        'data': './data/routes.geojson'
-    });
-
-    map.addLayer({
-        'id': 'lines',
-        'type': 'line',
-        'source': 'routes',
-        'paint': {
-            'line-width': 3,
-            'line-color': ['get', 'color']
+    /*************************************** ROUTING LAYER ***************************************/
+//    map.addSource('routes', {
+//        'type': 'geojson',
+//        'data': './data/routes.geojson'
+//    });
+//
+//    map.addLayer({
+//        'id': 'lines',
+//        'type': 'line',
+//        'source': 'routes',
+//        'paint': {
+//            'line-width': 3,
+//            'line-color': ['get', 'color']
+//        }
+//    });
+    
+    /*************************************** SYMBOL LAYER ***************************************/
+    d3.json('./data/routes.geojson', function(err, data) {
+        if (err) throw err;
+        
+        var coordinateList = [];
+        for (var i = 0; i < data.features.length; i++) {
+            // save full coordinate list for later
+            coordinateList[i] = data.features[i].geometry.coordinates;
+            
+            // start by showing just the first coordinate
+            data.features[i].geometry.coordinates = [coordinateList[i][0]];
+            var featurePoint = JSON.parse('{"type": "FeatureCollection", "features": [' + JSON.stringify(data.features[i]) + ']}');
+            console.log(featurePoint);
+            // add it to the map
+            map.addSource('trace' + i, { type: 'geojson', data: featurePoint });
+            map.addLayer({
+                "id": "trace" + i,
+                "type": "line",
+                "source": "trace" + i,
+                "paint": {
+                    "line-color": ['get', 'color'],
+                    "line-opacity": 0.75,
+                    "line-width": 5
+                }
+            });
         }
+        // setup the viewport
+//        map.jumpTo({ 'center': coordinates[0], 'zoom': 14 });
+//        map.setPitch(30);
+
+        // on a regular basis, add more coordinates from the saved list and update the map
+        var j = 0;
+        var timer = window.setInterval(function() {
+            for (var i = 0; i < coordinateList.length; i++) {
+                if (j < coordinateList[i].length) {
+                    data.features[i].geometry.coordinates.push(coordinateList[i][j]);
+                    map.getSource('trace0').setData(data);
+                    //map.panTo(coordinateList[i][j]);
+                    j++;
+                } else {
+                    window.clearInterval(timer);
+                }
+            }
+        }, 1000);
+    });
+    map.loadImage('https://png.icons8.com/metro/1600/interstate-truck.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('truck', image);
+        /*************************************** SYMBOL LAYER ***************************************/
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [0, 0]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "truck",
+                "icon-size": 0.1,
+                "icon-rotate": 180
+            }
+        });
     });
 });
 
